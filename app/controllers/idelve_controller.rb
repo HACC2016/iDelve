12995381bced10176f7e3b9cecde26e8511db6f9
Andrew Wong <coding.monkey.andrew@gmail.com>
2016-09-11 15:49:54 -1000

    add user authentication requirement.  update root route to point to this controller

--- app/controllers/idelve_controller.rb
+++ app/controllers/idelve_controller.rb
@@ -1,4 +1,6 @@
 class IdelveController < ApplicationController
+  before_action :authenticate_user!
+
   def new
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "user#sign_in"
+  root to: "idelve#show"
 end
